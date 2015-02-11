a9bae6710ce92ee2c8ba212cf5041713585fefb5
pablojan <pablojan@pablojan@gmail.com>
2015-02-11 17:01:50 +0100

    Sample http client with threads

--- src/org/waveprotocol/android/WaveAndroid.java
+++ src/org/waveprotocol/android/WaveAndroid.java
@@ -1,5 +1,7 @@
 package org.waveprotocol.android;
 
+import org.waveprotocol.android.client.WaveAndroidClient;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
@@ -8,45 +10,108 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gwt.core.client.Callback;
 
 public class WaveAndroid extends Activity implements OnClickListener {
 
-	private EditText eUser;
-	private EditText ePassword;
-	private EditText eServer;
+  private EditText eUser;
+  private EditText ePassword;
+  private EditText eServer;
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		Button butttonLogin = (Button) findViewById(R.id.buttonLogin);
-		butttonLogin.setOnClickListener(this);
-	}
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    Button butttonLogin = (Button) findViewById(R.id.buttonLogin);
+    butttonLogin.setOnClickListener(this);
+  }
 
-	@Override
-	public void onClick(View arg0) {
+  @Override
+  public void onClick(View arg0) {
 
-		eUser = (EditText) findViewById(R.id.editTextUser);
-		ePassword = (EditText) findViewById(R.id.editTextPassword);
-		eServer = (EditText) findViewById(R.id.editTextServer);
+    eUser = (EditText) findViewById(R.id.editTextUser);
+    ePassword = (EditText) findViewById(R.id.editTextPassword);
+    eServer = (EditText) findViewById(R.id.editTextServer);
 
-		String user = eUser.getText().toString();
-		String pass = ePassword.getText().toString();
-		String server = eServer.getText().toString();
+    String user = eUser.getText().toString();
+    String pass = ePassword.getText().toString();
+    String server = eServer.getText().toString();
 
-		if (!user.isEmpty() && !pass.isEmpty() && !server.isEmpty()) {
-			Intent i = new Intent(this, WaveAndroidSession.class);
-			i.putExtra("user", user);
-			i.putExtra("pass", pass);
-			i.putExtra("server", server);
-			startActivity(i);
-		}
+    if (!user.isEmpty() && !pass.isEmpty() && !server.isEmpty()) {
 
-		else {
-			AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-			alertDialog.setMessage("You must complete all fields");
-			alertDialog.show();
-		}
-	}
+      Thread loginThread = new Thread(new LoginRunnable(user, pass, server));
+      loginThread.start();
+
+    }
+
+    else {
+      AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+      alertDialog.setMessage("You must complete all fields");
+      alertDialog.show();
+    }
+  }
+
+  private class LoginRunnable implements Runnable {
+
+    final String user, password, server;
+
+    public LoginRunnable(String user, String password, String server) {
+      this.user = user;
+      this.password = password;
+      this.server = server;
+    }
+
+    @Override
+    public void run() {
+
+      final WaveAndroidClient waveClient = new WaveAndroidClient();
+
+      waveClient.startSession(user, password, server, new Callback<String, String>() {
+
+        @Override
+        public void onSuccess(String result) {
+
+          WaveAndroid.this.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+
+              Intent i = new Intent(WaveAndroid.this, WaveAndroidSession.class);
+              i.putExtra("user", user);
+              i.putExtra("pass", password);
+              i.putExtra("server", server);
+              startActivity(i);
+
+            }
+          });
+
+        }
+
+        @Override
+        public void onFailure(final String reason) {
+
+          WaveAndroid.this.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+
+              Toast
+.makeText(WaveAndroid.this.getApplicationContext(),
+                  "Failed Wave server connection: " + reason, Toast.LENGTH_LONG)
+                  .show();
+
+            }
+          });
+
+        }
+
+      });
+
+    }
+  }
+
+
 }
--- src/org/waveprotocol/android/WaveAndroidSession.java
+++ src/org/waveprotocol/android/WaveAndroidSession.java
@@ -5,51 +5,30 @@ import org.waveprotocol.android.client.WaveAndroidClient;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.google.gwt.core.client.Callback;
 
 public class WaveAndroidSession extends Activity {
 
-	private String user;
-	private String password;
-	private String server;
-	private WaveAndroidClient waveClient;
+  private String user;
+  private String password;
+  private String server;
+  private WaveAndroidClient waveClient;
 
-	private Context context;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.session);
-		this.getUserData();
-		
-		this.context = getApplicationContext();
+  private Context context;
 
-		waveClient = new WaveAndroidClient();
-		waveClient.startSession(user, password, server,
-				new Callback<String, String>() {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.session);
+    this.getUserData();
 
-					@Override
-					public void onSuccess(String result) {
+    this.context = getApplicationContext();
+  }
 
-						Toast.makeText(context, "Connected to Wave server with session Id: "
-										+ result, Toast.LENGTH_LONG).show();
-					}
-
-					@Override
-					public void onFailure(String reason) {
-						Toast.makeText(context, "Failed Wave server connection: " + reason,
-								Toast.LENGTH_LONG).show();
-					}
-				});
-	}
-
-	private void getUserData() {
-		Bundle extras = getIntent().getExtras();
-		this.user = extras.getString("user");
-		this.password = extras.getString("pass");
-		this.server = extras.getString("server");
-	}
+  private void getUserData() {
+    Bundle extras = getIntent().getExtras();
+    this.user = extras.getString("user");
+    this.password = extras.getString("pass");
+    this.server = extras.getString("server");
+  }
 
 }
